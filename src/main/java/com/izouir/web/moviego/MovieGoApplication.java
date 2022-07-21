985e89837f923d3ced642c1eca5aa69622abfa72
Timofey Kozlov <iZouiR@inbox.ru>
2022-07-21 17:32:21 +0300

    init commit

---
+++ src/main/java/com/izouir/web/moviego/MovieGoApplication.java
@@ -0,0 +1,13 @@
+package com.izouir.web.moviego;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieGoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovieGoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/izouir/web/moviego/MovieGoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.izouir.web.moviego;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieGoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
